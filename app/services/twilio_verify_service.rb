0ce69cfa0f7d1dc84755877f52790a052768195b
Jay Wolff <jaywolff16@gmail.com>
2023-03-06 16:53:24 -0500

    refactor to accept rails credentials or fallback to env vars for twilio account

--- app/services/twilio_verify_service.rb
+++ app/services/twilio_verify_service.rb
@@ -1,5 +1,5 @@
 class TwilioVerifyService
-  attr_reader :twilio_client
+  attr_reader :twilio_client, :twilio_account_sid, :twilio_auth_token, :twilio_verify_service_sid
 
   def self.send_sms_token(phone_number)
     new.twilio_verify_service.verifications.create(to: e164_format(phone_number), channel: 'sms')
@@ -15,11 +15,17 @@ class TwilioVerifyService
   end
 
   def initialize
-    @twilio_client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+    @twilio_account_sid = Rails.application.credentials[:twilio_account_sid] || ENV['TWILIO_ACCOUNT_SID']
+    @twilio_auth_token = Rails.application.credentials[:twilio_auth_token] || ENV['TWILIO_AUTH_TOKEN']
+    @twilio_verify_service_sid = Rails.application.credentials[:twilio_verify_service_sid] || ENV['TWILIO_VERIFY_SERVICE_SID']
+
+    raise 'Missing Twilio credentials' unless @twilio_account_sid && @twilio_auth_token && @twilio_verify_service_sid
+
+    @twilio_client = Twilio::REST::Client.new(@twilio_account_sid, @twilio_auth_token)
   end
 
   def twilio_verify_service
-    twilio_client.verify.services(ENV['TWILIO_VERIFY_SERVICE_SID'])
+    twilio_client.verify.services(twilio_verify_service_sid)
   end
 
   def e164_format(phone_number)
